6539e0d4ed2544dc14ebd0b649555f4b9ce01e0b
Yiğit Cankurtaran <yigitcankurtaranyc@gmail.com>
2024-07-06 23:46:23 +0300

    finished the first few exercises

--- hello.swift
+++ hello.swift
@@ -1,1 +1,21 @@
-print("hello world")
+print("hello world\n")
+
+var myVar = 42
+myVar = 42
+let myConst  = 50
+print("the variable is \(myVar) and the constant is \(myConst)\n")
+
+let implicitConstFloat: Float = 4.0
+print("the implicit constant is \(implicitConstFloat)\n")
+
+var fruits = ["apple", "banana", "orange"]
+print(fruits[1])
+
+let randomDict: [String : String] = ["key1": "value1", "key2": "value2"]
+// both strings
+print(randomDict["key1"]!)
+// have to use ! here because it is an optional
+// to make it a non-optional, use the following
+
+var DictWithNumbers: [String : Int] = ["key1": 1, "key2": 2]
+print(DictWithNumbers["key1"]!)
