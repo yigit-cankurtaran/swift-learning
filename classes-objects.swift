f6ca5f6222122b7fd51b95a599cdbd56f0d88980
Yiğit Cankurtaran <yigitcankurtaranyc@gmail.com>
2024-07-29 10:22:16 +0300

    deeper dive into objects

--- classes-objects.swift
+++ classes-objects.swift
@@ -1,12 +1,15 @@
 class Shape {
+    // blueprint for the object
     var numberOfSides: Int = 0
     let name: String = "Shape"
     func simpleDesc() -> String {
         return "A shape with \(numberOfSides) sides."
     }
+    // method
     func oddOrEven() -> Bool {
         return numberOfSides % 2 == 0
     }
+    // method
 }
 
 var myShape: Shape = Shape()
@@ -14,6 +17,7 @@ var myShape: Shape = Shape()
 myShape.numberOfSides = 7
 print(myShape.simpleDesc())
 print(myShape.name)
+// this prints Shape. the current class doesn't have a name
 print(myShape.oddOrEven())
 
 class NamedShape {
@@ -48,6 +52,7 @@ class Square: NamedShape {
 }
 
 let test = Square(sideLength: 5.2, name: "Spongebob")
+// these are the initialization variables we set with the init thing
 print(test.area())
 print(test.simpleDesc())
 
@@ -56,6 +61,8 @@ class Circle: NamedShape {
     init(radius: Double, name: String) {
         self.radius = radius
         super.init(name: name)
+        // the super probably comes from the superclass.
+        // we pass that to the superclass because this is the NamedShape
         numberOfSides = 1
     }
     func area() -> Double {
@@ -65,8 +72,42 @@ class Circle: NamedShape {
     override func simpleDesc() -> String {
         return "A circle with a radius of \(radius) named \(name)"
     }
+    // overrides the superclass's simpleDesc.
 }
 
 let testCircle = Circle(radius: 5.2, name: "Patrick")
 print(testCircle.area())
 print(testCircle.simpleDesc())
+
+// we can also have getters and setters
+class EquilateralTriangle: NamedShape {
+    var sideLength: Double
+
+    init (sideLength: Double, name: String) {
+        self.sideLength = sideLength
+        super.init(name: name)
+        numberOfSides = 3
+    }
+
+    var perimeter: Double {
+        get {
+            return 3.0 * sideLength
+        }
+        set {
+            sideLength = newValue / 3.0
+            // if we pass something new to the perimeter we can set sidelength with it
+            // newValue is the default name, how do we change this?
+
+        }
+    }
+
+    override func simpleDesc() -> String {
+        return "An equilateral triangle with sides of length \(sideLength)"
+    }
+
+}
+
+var triangle = EquilateralTriangle(sideLength: 3.1, name: "a triangle")
+print(triangle.perimeter)
+triangle.perimeter = 9.9
+print(triangle.sideLength)
