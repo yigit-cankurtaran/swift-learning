7e9a18b7a4effe30742609cd6351ed92c3316be8
Yiğit Cankurtaran <yigitcankurtaranyc@gmail.com>
2024-07-29 09:26:39 +0300

    flow control example

--- flowcontrol.swift
+++ flowcontrol.swift
@@ -1,7 +1,9 @@
 var optionalString: String? = "Hello"
 print(optionalString == nil)
+// prints false
 
-var optionalName: String? = nil
+var optionalName: String? = "me"
+// this question mark marks this variable as optional
 var greeting = "Hello!"
 // this is the default value
 if let name = optionalName {
@@ -10,8 +12,21 @@ if let name = optionalName {
 }
 print("\(greeting)")
 
-let nickName: String? = "heyo"
+let nickName: String? = nil
 var fullName: String = "John Appleseed"
 var informalGreeting = "Hi \(nickName ?? fullName)"
 // if nickName is nil, then use fullName
 print(informalGreeting)
+
+let individualScores = [75, 43, 103, 87, 12]
+// an int array
+var teamScore = 0
+// initializing a variable to store the result in
+for score in individualScores {
+    if score > 50 {
+        teamScore += 3
+    } else {
+        teamScore += 1
+    }
+}
+print(teamScore)
